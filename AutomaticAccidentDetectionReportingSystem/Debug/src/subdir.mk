7da996cf379aa5a23326e3ed179f5e3a5bab870e
RosianRad <rad.rosian@gmail.com>
2017-03-26 14:36:24 +0300

    Added chip and board libraries

--- AutomaticAccidentDetectionReportingSystem/Debug/src/subdir.mk
+++ AutomaticAccidentDetectionReportingSystem/Debug/src/subdir.mk
@@ -26,7 +26,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -DDEBUG -D__CODE_RED -DCORE_M0 -D__USE_LPCOPEN -D__LPC11XX__ -D__REDLIB__ -I"C:\Users\rosian.rad\Documents\LPCXpresso_8.1.4_606\workspace\AutomaticAccidentDetectionReportingSystem\inc" -I"C:\Users\rosian.rad\Documents\LPCXpresso_8.1.4_606\workspace\nxp_lpcxpresso_11c24_board_lib\inc" -I"C:\Users\rosian.rad\Documents\LPCXpresso_8.1.4_606\workspace\lpc_chip_11cxx_lib\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-gcc -DDEBUG -D__CODE_RED -DCORE_M0 -D__USE_LPCOPEN -D__LPC11XX__ -D__REDLIB__ -I"C:\Work\Licenta\git\AutomaticAccidentDetectionReportingSystem\AutomaticAccidentDetectionReportingSystem\inc" -I"C:\Work\Licenta\git\AutomaticAccidentDetectionReportingSystem\nxp_lpcxpresso_11c24_board_lib\inc" -I"C:\Work\Licenta\git\AutomaticAccidentDetectionReportingSystem\lpc_chip_11cxx_lib\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
